1191d9054b786218897f1f380c28d950629ec6f5
Alex Nichol <unixpickle@gmail.com>
2018-05-05 18:52:48 -0700

    RNNCell passes

--- wavenet/cell.py
+++ wavenet/cell.py
@@ -2,7 +2,12 @@
 RNN cells for WaveNet components.
 """
 
+import tensorflow as tf
 from tensorflow.contrib.rnn import RNNCell  # pylint: disable=E0611
+
+# Disable warning about "compute_output_shape" not being
+# overridden, since most RNNCells don't seem to do so.
+# pylint: disable=W0223
 
 
 class ConvCell(RNNCell):
@@ -15,6 +20,7 @@ class ConvCell(RNNCell):
         """
         Create a ConvCell from a Conv instance.
         """
+        super(ConvCell, self).__init__()
         self.conv = conv
 
     @property
@@ -32,7 +38,8 @@ class ConvCell(RNNCell):
         zeros = tf.zeros([batch_size, self.conv.in_depth], dtype=dtype)
         return (zeros,) * (self.conv.receptive_field - 1)
 
-    def call(self, inputs, state):
+    def call(self, inputs, state):  # pylint: disable=W0221
         old_inputs = state[0]
         new_cache = state[1:] + (inputs,)
-        return self.conv.apply_once(old_inputs, inputs)
+        outputs = self.conv.apply_once(old_inputs, inputs)
+        return outputs, new_cache
---
+++ wavenet/test_cells.py
@@ -0,0 +1,25 @@
+"""
+Test RNN cells.
+"""
+
+import numpy as np
+import tensorflow as tf
+
+from .network import Conv, Network
+
+
+def test_cell_equivalence():
+    """
+    Test that an RNNCell for a network gives the same
+    output as the network itself.
+    """
+    with tf.Graph().as_default():
+        net = Network([Conv(5, i, dtype=tf.float64) for i in range(3)])
+        assert net.receptive_field == 8
+        in_seq = tf.concat([tf.random_normal([3, 9, 5], dtype=tf.float64)] * 2, axis=1)
+        actual, _ = tf.nn.dynamic_rnn(net.cell(), in_seq, dtype=tf.float64)
+        expected = net.apply(in_seq)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            actual, expected = sess.run((actual, expected))
+            assert np.allclose(actual, expected)
