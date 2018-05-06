e432938212b7f56ebf43d15d405d9da259ac1871
Alex Nichol <unixpickle@gmail.com>
2018-05-05 17:41:15 -0700

    test apply()

--- wavenet/test_network.py
+++ wavenet/test_network.py
@@ -2,6 +2,7 @@
 Tests for networks.
 """
 
+import numpy as np
 import tensorflow as tf
 
 from .network import Conv, Network
@@ -20,3 +21,27 @@ def test_network_rf():
 
         net = Network([Conv(7, 2), Conv(7, 0), Conv(7, 1), Conv(7, 0)])
         assert net.receptive_field == 9
+
+
+def test_network_apply():
+    """
+    Test applying a WaveNet model.
+
+    This is mostly intended to catch runtime errors.
+    """
+    with tf.Graph().as_default():
+        net = Network([Conv(5, i, dtype=tf.float64) for i in range(3)])
+        assert net.receptive_field == 8
+        # A sequence that repeats at an interval that is
+        # greater than the receptive field.
+        in_seq = tf.concat([tf.random_normal([3, 9, 5], dtype=tf.float64)] * 2, axis=1)
+        out_seq = net.apply(in_seq)
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            outputs = sess.run(out_seq)
+            # The same, because receptive fields are identical.
+            assert np.allclose(outputs[:, 8], outputs[:, 17])
+            # Not the same because of being different timesteps.
+            assert not np.allclose(outputs[:, 8], outputs[:, 16])
+            # Not the same because of zero padding.
+            assert not np.allclose(outputs[:, 0], outputs[:, 9])
